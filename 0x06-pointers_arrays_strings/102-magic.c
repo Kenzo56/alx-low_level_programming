15fcf119054b28f434d3f2089f245d3095cc896f
Kenzo56 <kenzfassi5@hotmail.com>
2023-06-29 17:40:00 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -6,9 +6,7 @@
 * - you are not allowed to modify p
 * - only one statement
 * - you are not allowed to code anything else than this line of code
-*/
-*(p + 5) = 98;
-/* ...so that this prints 98\n */
+* ...so that this prints 98\n */
 int main(void)
 {
 int n;
